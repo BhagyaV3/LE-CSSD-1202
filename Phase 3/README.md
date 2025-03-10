ebba472e18b892ea736f15d4e50683792ee26132
Shlok Desai <shlok64@my.yorku.ca>
2025-03-10 18:41:54 -0400

    QOL Changes

--- Phase 3/README.md
+++
@@ -1,4 +0,0 @@
-NOTES:
-
-- We only had time for some of the media queries.
-- The send message part in the Contact page was deleted foe the iPhone mode because it couldn't completely in one screen.
--- Phase 3/scripts/login.js
+++ Phase 3/scripts/login.js
@@ -1,3 +1,30 @@
+var loginInfo = [
+    {
+        email: "shlok@gmail.com",
+        password: "shlok123"
+    },
+    {
+        email: "bhagya@gmail.com",
+        password: "bhag111"
+    }
+];
+
+function getInfo(event) {
+    var email = document.getElementById('emailInput').value;
+    var password = document.getElementById('passwordInput').value;
+
+    for (var i = 0; i < loginInfo.length; i++) {
+        if (email == loginInfo[i].email && password == loginInfo[i].password) {
+            console.log(email + " is logged in!!!");
+            location.replace("./tracker.html");
+            return;
+        }
+    }
+    console.log("Incorrect email or password");
+    alert("Incorrect email or password! Please try again.");
+}
+
+
 const showPassBox = document.getElementById('showPassBox');
         const passwordInput = document.getElementById('passwordInput');
 
