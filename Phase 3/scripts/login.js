2f2385c7170487b4fa604121237ca4dc904026b5
Shlok Desai <shlok64@my.yorku.ca>
2025-03-23 13:00:40 -0400

    mo

--- Phase 3/scripts/login.js
+++ Phase 3/scripts/login.js
@@ -23,7 +23,10 @@ function makeOTP() {
     password = document.getElementById('passwordInput').value;
     nameRegEx = /[A-Z][a-z]+\s?[A-Z][a-z]+/;
     emailRegEx = /[a-z]+\@[a-z]+\.[a-z]+/;
-    paswordRegEx = /(?=.*[A-Z])(?=.*[a-z])(?=.*\W)(?=.*\d)/;
+    paswordRegEx = /[^a-zA-Z0-9]/;
+
+
+    console.log(paswordRegEx.test(password));
 
     if (!(nameRegEx.test(nameJ))) {
         alert("Invalid name format. Please type your name like this: First Last");
