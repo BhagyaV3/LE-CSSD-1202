46d47362801c9ad1a9e3ce4433a825db19225e66
Bhagya221473145 <bhvithanage@gmail.com>
2025-03-24 01:15:16 -0400

    Slight edits

---
+++ Phase 3/scripts/calendar.js
@@ -0,0 +1,52 @@
+const monthYear = document.getElementById('currentDateC');
+const dates = document.getElementById('datesC');
+const leftArrow = document.getElementById('leftArrowC');
+const rightArrow = document.getElementById('rightArrowC');
+
+let currentDate = new Date();
+
+const updateCalendar = () => {
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth();
+    const firstDay = new Date(currentYear, currentMonth, 0);
+    const lastDay = new Date(currentYear, currentMonth + 1, 0);
+    const totalDays = lastDay.getDate();
+    const firstDayIndex = firstDay.getDay();
+    const lastDayIndex = lastDay.getDay();
+    const monthYearString = currentDate.toLocaleDateString
+    ('default', {month: 'long', year: 'numeric'});
+    monthYear.textContent = monthYearString;
+
+    let datesHTML = '';
+
+    for (let i = firstDayIndex; i > 0; i--) {
+        const prevDate = new Date(currentYear, currentMonth, 0 - i + 1);
+        datesHTML += `<div class="date inactive">${prevDate.getDate()}</div>`;
+    }
+
+    for (let i = 1; i <= totalDays; i++) {
+        const date = new Date(currentYear, currentMonth, i);
+        const activeClass = date.toDateString() === new Date().
+        toDateString() ? 'active' : '';
+        datesHTML += `<div class="date ${activeClass}"></div>`;
+    }
+
+    for (let i = 1; i <= 7 - lastDayIndex; i++) {
+        const nextDate = new Date(currentYear, currentMonth + 1, i);
+        datesHTML += `<div class="date inactive">${nextDate.getDate()}</div`;
+    }
+
+    datesElement.innerHTML = datesHTML;
+
+    leftArrow.addEventListener('click', () => {
+        currentDate.setMonth(currentDate.getMonth() - 1);
+        updateCalendar();  
+    })
+
+    rightArrow.addEventListener('click', () => {
+        currentDate.setMonth(currentDate.getMonth() + 1);
+        updateCalendar();  
+    })
+
+    updateCalendar();
+}
